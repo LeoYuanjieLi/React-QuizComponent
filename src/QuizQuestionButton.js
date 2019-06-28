162abdaec8a08dc6d96a33407f0c3f7bdcc2d551
Leo Li <leo.li@adidas.com>
2019-06-27 23:14:56 -0700

    button

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuizQuestionButton extends Component {
+    render() {
+        return (
+
+            <li><button>Test</button></li>
+        )
+    }
+
+}
+
+export default QuizQuestionButton;
