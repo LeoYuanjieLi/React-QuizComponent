3dececf46aa7871a50065a266b554ff88410d7da
Leo Li <leo.li@adidas.com>
2019-07-11 23:58:03 -0700

    fixed quiz_position issue

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,9 +10,10 @@ class Quiz extends Component {
         super(props);
         this.state = {quiz_position: 1}
     };
+    
     render() {
-        const isQuizEnd = ((this.state.quiz_question - 1)  === 
-        quizData.quiz_questions.length);
+        const isQuizEnd = (this.state.quiz_position - 1  === quizData.quiz_questions.length)
+
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> : 
